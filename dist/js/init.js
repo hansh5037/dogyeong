2aa116dd7a76a872d523b1fffb32b42c311e6248
hansh5037 <hansh5037@naver.com>
2025-10-21 00:48:38 +0900

    fix : 일단 기능 구현만

--- dist/js/init.js
+++ dist/js/init.js
@@ -2,6 +2,7 @@
 window.component = window.component || {};
 window.component.kv = /* @__PURE__ */ (function() {
   let els = {};
+  let gap = 0.1;
   const init = function() {
     els.section = document.querySelector(".component-kv");
     if (!!els.section) {
@@ -18,25 +19,81 @@ window.component.kv = /* @__PURE__ */ (function() {
   const bindEvents = function() {
     eventsList.setFixedScroll();
     eventHandler.scroll();
+    eventHandler.wheel();
   };
   const eventHandler = {
     scroll: function() {
       window.addEventListener("scroll", function() {
         const p = eventsList.getScroll();
-        console.log(Math.round(p * 100));
+      });
+    },
+    wheel: function() {
+      window.addEventListener("wheel", (event) => {
+        const p = eventsList.getScroll();
+        const inRange = p > 0 && p < 1;
+        const wheel = event.wheelDeltaY;
+        if (inRange) {
+          if (wheel > 0) {
+            eventsList.headlineBottomAnimation();
+          } else {
+            eventsList.headlineTopAnimation();
+          }
+        }
+        return;
       });
     }
   };
   const eventsList = {
     setFixedScroll: function() {
-      els.fixedTrack.style.height = window.innerHeight * 3 + "px";
+      els.fixedTrack.style.height = window.innerHeight * 5 + "px";
     },
     getScroll: function() {
       const elsRect = els.fixedTrack.getBoundingClientRect();
       const viewPort = window.visualViewport?.height || window.innerHeight;
-      const elsHeight = elsRect.height;
-      const t = (viewPort - elsRect.top) / elsHeight;
+      const t = elsRect.top / (viewPort - elsRect.height);
       return Math.max(0, Math.min(1, t));
+    },
+    getScaleValue: function(repeat = 1, eps = 1e-3) {
+      const scroll = eventsList.getScroll();
+      const x = scroll * repeat % 1;
+      let v = (1 - Math.cos(2 * Math.PI * x)) / 2;
+      if (v > 1 - eps) v = 1;
+      else if (v < eps) v = 0;
+      return v;
+    },
+    headlineTopAnimation: function() {
+      const alphabetTop = els.headlineTop.querySelectorAll("span");
+      const alphabetBottom = els.headlineBottom.querySelectorAll("span");
+      const scaleValue = eventsList.getScaleValue();
+      for (let i = 0; i < alphabetTop.length; i++) {
+        let scale = 1 - (scaleValue + gap * i);
+        if (scale < 0) scale = 0;
+        if (scale > 1) scale = 1;
+        alphabetTop[i].style.transform = `scaleY(${scale})`;
+      }
+      for (let i = 0; i < alphabetBottom.length; i++) {
+        let scale = scaleValue + gap * i;
+        if (scale < 0) scale = 0;
+        if (scale > 1) scale = 1;
+        alphabetBottom[i].style.transform = `scaleY(${scale})`;
+      }
+    },
+    headlineBottomAnimation: function() {
+      const alphabetTop = els.headlineTop.querySelectorAll("span");
+      const alphabetBottom = els.headlineBottom.querySelectorAll("span");
+      const scaleValue = eventsList.getScaleValue();
+      for (let i = 0; i < alphabetTop.length; i++) {
+        let scale = scaleValue + gap * i;
+        if (scale < 0) scale = 0;
+        if (scale > 1) scale = 1;
+        alphabetTop[i].style.transform = `scaleY(${scale})`;
+      }
+      for (let i = 0; i < alphabetBottom.length; i++) {
+        let scale = 1 - (scaleValue + gap * i);
+        if (scale < 0) scale = 0;
+        if (scale > 1) scale = 1;
+        alphabetBottom[i].style.transform = `scaleY(${scale})`;
+      }
     }
   };
   return {
