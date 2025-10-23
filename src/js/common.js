4c94483e1dc0f665aaafbad332d9573fa5b03d5e
hansh5037 <hansh5037@naver.com>
2025-10-24 00:20:47 +0900

    fix > 수정

--- src/js/common.js
+++ src/js/common.js
@@ -12,21 +12,22 @@ window.component.common = (function () {
 	};
 
 	const setElemets = function () {
-        els.cursor = els.section.querySelector('.common-cursor')
-    };
+		els.cursor = els.section.querySelector('.common-cursor')
+	};
 	const BindEvents = function () {
-        eventHandler.mouseMove();
-    };
+		if (!window.matchMedia('(hover: hover) and (pointer: fine)').matches) return;
+		eventHandler.mouseMove();
+	};
 	const eventHandler = {
-        mouseMove: function () {
-            document.addEventListener("mousemove", function (event) {
-                let x = event.clientX;
-                let y = event.clientY;
+		mouseMove: function () {
+			document.addEventListener("mousemove", function (event) {
+				let x = event.clientX;
+				let y = event.clientY;
 
-                els.cursor.style.cssText = "left:"+x+"px;"+"top:"+y+"px;"
-            })
-        }
-    };
+				els.cursor.style.cssText = "left:"+x+"px;"+"top:"+y+"px;"
+			})
+		}
+	};
 	const eventsList = {};
 	return {
 		init: init
