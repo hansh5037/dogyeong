d3cd28ec982246811796b54985d4ef8919f2fbd3
hansh5037 <hansh5037@naver.com>
2025-10-29 18:08:21 +0900

    fix : 이미지 추가

--- src/js/kv.js
+++ src/js/kv.js
@@ -1,88 +1,106 @@
 window.component = window.component || {};
 window.component.kv = (function () {
-    let els = {};
-    let gap = 0.1;
-    let fixed = window.component.common.fixedScrollEvent;
+	let els = {};
+	let gap = 0.1;
+	let fixed = window.component.common.fixedScrollEvent;
 
-    const clamp01 = v => Math.max(0, Math.min(1, v));
+	const clamp01 = v => Math.max(0, Math.min(1, v));
 
-    const init = function () {
-        els.section = document.querySelector('.component-kv');
+	const init = function () {
+		els.section = document.querySelector('.component-kv');
 
-        if (!!els.section) {
-            setElements();
-            bindEvents();
-        }
-    };
+		if (!!els.section) {
+			setElements();
+			bindEvents();
+		}
+	};
 
-    const setElements = function () {
-        els.headlineTop = els.section.querySelector('.js-headline-top');
-        els.headlineBottom = els.section.querySelector('.js-headline-bottom');
-        els.alphabetTop = els.headlineTop.querySelectorAll('span');
-        els.alphabetBottom = els.headlineBottom.querySelectorAll('span');
+	const setElements = function () {
+		els.fixedInner = els.section.querySelector('.common-fixed-inner')
+		els.headlineTop = els.section.querySelector('.js-headline-top');
+		els.headlineBottom = els.section.querySelector('.js-headline-bottom');
+		els.alphabetTop = els.headlineTop.querySelectorAll('span');
+		els.alphabetBottom = els.headlineBottom.querySelectorAll('span');
 
-        els.eyebrow = els.section.querySelector('.js-eyebrow');
-    };
+		els.eyebrow = els.section.querySelector('.js-eyebrow');
 
-    const bindEvents = function () {
-        eventHandler.scroll();
-        eventsList.eyebrowChange();
-    };
+		els.bg = els.section.querySelector('.js-background');
+		els.bgImg = els.bg.querySelector('img');
+	};
 
-    const eventHandler = {
-        scroll: function () {
-            window.addEventListener('scroll', function () {
-                const p = fixed.getProgress();
-                const dir = fixed.getDirection();
+	const bindEvents = function () {
+		eventHandler.scroll();
+		eventsList.eyebrowChange();
+		eventsList.fixedOverflowVisible();
+	};
+
+	const eventHandler = {
+		scroll: function () {
+			window.addEventListener('scroll', function () {
+				const p = fixed.getProgress();
+				const dir = fixed.getDirection();
 				if (p.trackInside) {
 					eventsList.top(p, dir);
 					eventsList.bottom(p, dir);
 				}
-            });
-        }
-    };
+			});
+		}
+	};
 
-    const eventsList = {
-        top: function (p, dir) {
+	const eventsList = {
+		top: function (p, dir) {
 			let prog = 1 - p.progress;
 
 			const spanArray = Array.from(els.alphabetTop);
 			const span = (dir === 'up') ? spanArray.reverse() : spanArray;
 			
-            for (let i = 0; i < span.length; i++) {
-                const scale = clamp01(prog);
+			for (let i = 0; i < span.length; i++) {
+				const scale = clamp01(prog);
 				if (dir === 'up') prog += gap;
 				if (dir === 'down') prog -= gap;
-                span[i].style.transform = `scaleY(${scale})`;
-            }
-        },
-        bottom: function (p, dir) {
-            let prog = p.progress;
+				span[i].style.transform = `scaleY(${scale})`;
+			}
+		},
+		bottom: function (p, dir) {
+			let prog = p.progress;
 
 			const spanArray = Array.from(els.alphabetBottom);
 			const span = (dir === 'up') ?  spanArray : spanArray.reverse();
 
-            for (let i = span.length - 1; i >= 0; i--) {
-                const scale = clamp01(prog);
+			for (let i = span.length - 1; i >= 0; i--) {
+				const scale = clamp01(prog);
 				if (dir === 'up') prog -= gap;
 				if (dir === 'down') prog += gap;
-                span[i].style.transform = `scaleY(${scale})`;
-            }
-        },
-        eyebrowChange: function () {
-            const eyebrowChangeValue = ['UI/UX', 'FrontEnd', 'Web'];
-            let i = 0;
+				span[i].style.transform = `scaleY(${scale})`;
+			}
+		},
+		eyebrowChange: function () {
+			const eyebrowChangeValue = ['UI/UX', 'FrontEnd', 'Web'];
+			let i = 0;
 
-            els.eyebrow.innerText = eyebrowChangeValue[i];
+			els.eyebrow.innerText = eyebrowChangeValue[i];
 
-            setInterval(function () {
-                i = (i + 1) % eyebrowChangeValue.length;
-                els.eyebrow.innerHTML = eyebrowChangeValue[i];
-            }, 1300);
-        }
-    };
+			setInterval(function () {
+				i = (i + 1) % eyebrowChangeValue.length;
+				els.eyebrow.innerHTML = eyebrowChangeValue[i];
+			}, 1300);
+		},
+		fixedOverflowVisible: function () {
+			const fixedHeight = els.fixedInner.clientHeight;
+			const bgHeight = els.bgImg.height;
+			console.log(els.fixedInner.clientHeight ,els.bgImg.height)
 
-    return {
-        init: init
-    }
+			if (fixedHeight < bgHeight) {
+				const paddingValue = bgHeight - fixedHeight;
+
+				els.fixedInner.style.paddingBottom = paddingValue + 'px';
+				els.bgImg.style.top = 0;
+				els.bgImg.style.bottom = 'auto';
+			}
+		}
+	};
+
+	return {
+		init: init
+	}
 })();
