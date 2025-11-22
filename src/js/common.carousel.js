c724ea45f80cfe86baf2385772fc202962500278
hansh5037 <hansh5037@naver.com>
2025-11-23 00:50:18 +0900

    refact: carousel > 변수명 변경, 위치 변경 등

--- src/js/common.carousel.js
+++ src/js/common.carousel.js
@@ -3,6 +3,7 @@ window.component.commonCarousel = (function () {
 
 	function Carousel(opts = {}) {
 		this.els = {};
+		this.state = {}; 
 		this.activeIndex = 0;
 		this.drag = {
 			isDown: false,
@@ -41,13 +42,15 @@ window.component.commonCarousel = (function () {
 		this.els.carouselPrevArrow = this.els.section.querySelector('.js-carousel-prev');
 		this.els.carouselPagination = this.els.section.querySelector('.js-carousel-pagination');
 		this.els.carouselPaginationBullet = null;
+		this.els.loopClonePrev = null;
+		this.els.loopCloneNext = null;
 	};
 
 	fn.setProperty = function () {
-		this.els.slideArray = [...this.els.carouselSlides];
-		this.els.lastIndex = this.els.carouselSlides.length - 1;
-		this.els.slideRect = this.els.carouselSlides[this.activeIndex].getBoundingClientRect();
-		this.els.slideWidth = this.els.slideRect.width;
+		this.state.slideArray = [...this.els.carouselSlides];
+		this.state.lastIndex = this.els.carouselSlides.length - 1;
+		this.state.slideRect = this.els.carouselSlides[this.activeIndex].getBoundingClientRect();
+		this.state.slideWidth = this.state.slideRect.width;
 	};
 
 	fn.bindEvents = function () {
@@ -55,7 +58,7 @@ window.component.commonCarousel = (function () {
 			this.carouselEventList.setPagination.call(this);
 		}
 		if (this.options.loop) {
-			this.carouselEventList.setCloneSlide.call(this);
+			this.carouselEventList.setLoopClones.call(this);
 		}
 		this.carouselEventList.slideChange.call(this);
 		this.eventHandler.on.call(this);
@@ -80,7 +83,7 @@ window.component.commonCarousel = (function () {
 
 			// drag 
 			this.els.carouselWrap.addEventListener('pointerdown', this.handler.dragStart, {passive:false});
-			window.addEventListener('pointermove', this.handler.dragMove);
+			this.els.carouselWrap.addEventListener('pointermove', this.handler.dragMove);
 			window.addEventListener('pointerup', this.handler.dragEnd);
 			window.addEventListener('pointercancel', this.handler.dragEnd);
 		},
@@ -93,7 +96,7 @@ window.component.commonCarousel = (function () {
 
 			// drag
 			this.els.carouselWrap.removeEventListener('pointerdown', this.handler.dragStart);
-			window.removeEventListener('pointermove', this.handler.dragMove);
+			this.els.carouselWrap.removeEventListener('pointermove', this.handler.dragMove);
 			window.removeEventListener('pointerup', this.handler.dragEnd);
 			window.removeEventListener('pointercancel', this.handler.dragEnd);
 
@@ -106,6 +109,7 @@ window.component.commonCarousel = (function () {
 			for (let i = 0; i < this.els.carouselSlides.length; i++) {
 				const bulletButtonWrap = document.createElement('li');
 				const bulletButton = document.createElement('button');
+				
 				bulletButton.type = 'button';
 				bulletButton.className = `js-carousel-bullet${i === 0 ? ' is-active' : ''}`;
 				bulletButton.setAttribute('aria-label', i + 1 + '/' + this.els.carouselSlides.length);
@@ -115,13 +119,55 @@ window.component.commonCarousel = (function () {
 			}
 			this.els.carouselPaginationBullet = this.els.carouselPagination.querySelectorAll('.js-carousel-bullet');
 		},
-		moveToTransform: function () { // clone이 아닌 슬라이드 위치(transform) 이동
-			let wrapperRect = this.els.carouselWrap.getBoundingClientRect().left;
-			let activeSlideRect = this.els.carouselSlides[this.activeIndex].getBoundingClientRect().left;
-			let rect = activeSlideRect - wrapperRect;
+		setLoopClones: function () { // loop용 clone slide 셋팅
+			const clones = [
+				this.state.slideArray[this.state.lastIndex].cloneNode(true),
+				this.state.slideArray[this.state.lastIndex - 1].cloneNode(true),
+				this.state.slideArray[0].cloneNode(true),
+				this.state.slideArray[1].cloneNode(true)
+			];
 
-			this.els.carouselWrap.style.transform = `translateX(-${rect}px)`;
-			return rect;
+			clones.forEach(clone => {
+				clone.setAttribute('aria-hidden', true);
+				clone.setAttribute('tabindex', '-1');
+			});
+
+			this.els.carouselWrap.prepend(clones[1], clones[0]);
+			this.els.carouselWrap.append(clones[2], clones[3]);
+
+			this.els.loopClonePrev = clones[0];
+			this.els.loopCloneNext = clones[2];
+		},
+		moveToTransform: function () { // clone이 아닌 슬라이드 위치(transform) 이동
+			const wrapperRect = this.els.carouselWrap.getBoundingClientRect().left;
+			const activeSlideRect = this.els.carouselSlides[this.activeIndex].getBoundingClientRect().left;
+			const diff = activeSlideRect - wrapperRect;
+
+			this.els.carouselWrap.style.transform = `translateX(-${diff}px)`;
+			return diff;
+		},
+		moveToClone: function (type, lastToFirst) {
+			const target = type === 'next' ? this.els.loopCloneNext : this.els.loopClonePrev;
+			const wrapperRect = this.els.carouselWrap.getBoundingClientRect().left;
+			const targetRect = target.getBoundingClientRect().left;
+			const diff = targetRect - wrapperRect;
+			const prevTransition = this.els.carouselWrap.style.transition;
+			const duration = parseFloat(window.getComputedStyle(this.els.carouselWrap).transitionDuration) * 1000;
+
+			this.els.carouselWrap.style.transform = `translateX(-${diff}px)`; // clone slide로 이동
+
+			// 실제 슬라이드로 이동(빠르게)
+			setTimeout(() => {
+				this.els.carouselWrap.style.transition = 'none';
+				this.activeIndex = lastToFirst ? 0 : this.state.lastIndex;
+				this.carouselEventList.slideChange.call(this);
+
+				this.els.carouselWrap.style.transition = prevTransition;
+			}, duration);
+
+			if (this.drag) {
+				this.drag.deltaX = 0;
+			}
 		},
 		toggleActiveClass: function (list) { // is-active 붙여주는 함수
 			for (let i = 0; i < list.length; i++) {
@@ -144,49 +190,6 @@ window.component.commonCarousel = (function () {
 			if (typeof this.options.onSlideChange === 'function') {
 				this.options.onSlideChange(this.activeIndex);
 			}
-		},
-		setCloneSlide: function () { // clone slide 셋팅
-			const slideArray = this.els.slideArray,
-				lastIndex = this.els.lastIndex,
-				carouselWrap = this.els.carouselWrap;
-
-			const clones = [
-				slideArray[lastIndex].cloneNode(true),
-				slideArray[lastIndex - 1].cloneNode(true),
-				slideArray[0].cloneNode(true),
-				slideArray[1].cloneNode(true)
-			];
-
-			clones.forEach(clone => {
-				clone.setAttribute('aria-hidden', true);
-				clone.setAttribute('tabindex', '-1');
-			});
-
-			carouselWrap.prepend(clones[1], clones[0]);
-			carouselWrap.append(clones[2], clones[3]);
-
-			this.els.loopClonePrev = clones[0];
-			this.els.loopCloneNext = clones[2];
-		},
-		moveToClone: function (type, lastToFirst) {
-			const target = type === 'next' ? this.els.loopCloneNext : this.els.loopClonePrev;
-			const wrapperRect = this.els.carouselWrap.getBoundingClientRect().left;
-			const targetRect = target.getBoundingClientRect().left;
-			const diff = targetRect - wrapperRect;
-			const prevTransition = this.els.carouselWrap.style.transition;
-			const duration = parseFloat(window.getComputedStyle(this.els.carouselWrap).transitionDuration) * 1000;
-
-			this.els.carouselWrap.style.transform = `translateX(-${diff}px)`;
-			setTimeout(() => {
-				this.els.carouselWrap.style.transition = 'none';
-				this.activeIndex = lastToFirst ? 0 : this.els.lastIndex;
-				this.carouselEventList.slideChange.call(this);
-
-				this.els.carouselWrap.style.transition = prevTransition;
-			}, duration);
-
-			this.drag.deltaX = 0;
-			return;
 		}
 	};
 
@@ -195,33 +198,29 @@ window.component.commonCarousel = (function () {
 			const isNext = event.currentTarget === this.els.carouselNextArrow;
 			const isPrev = event.currentTarget === this.els.carouselPrevArrow;
 
-			let nextIndex = this.activeIndex;
-
-			if (isNext) {
-				nextIndex += 1;
-			} else if (isPrev) {
-				nextIndex -= 1;
-			}
+			let nextIndex = this.activeIndex + (isNext ? 1 : -1);
 
 			const firstToLast = nextIndex < 0;
-			const lastToFirst = nextIndex > this.els.lastIndex;
+			const lastToFirst = nextIndex > this.state.lastIndex;
 
 			if (this.options.loop) {
 				if (firstToLast || lastToFirst) {
 					this.carouselEventList.moveToClone.call(this, firstToLast? 'prev' : 'next', lastToFirst); return;
 				};
-			} else if (nextIndex < 0 || nextIndex > this.els.lastIndex) return;
+			} else if (nextIndex < 0 || nextIndex > this.state.lastIndex) return;
 
-			this.activeIndex = nextIndex;
-			this.carouselEventList.slideChange.call(this);
+			if (nextIndex !== this.activeIndex) {
+				this.activeIndex = nextIndex;
+				this.carouselEventList.slideChange.call(this);
+			}
 		},
 		onBulletClick: function (event) {
 			const clickedBullet = event.target.closest('.js-carousel-bullet');
 			const clickBulletIndex = [...this.els.carouselPaginationBullet].indexOf(clickedBullet);
 
 			if (!clickedBullet || clickBulletIndex < 0) return;
+			
 			this.activeIndex = clickBulletIndex;
-
 			this.carouselEventList.slideChange.call(this);
 		}
 	};
@@ -236,8 +235,8 @@ window.component.commonCarousel = (function () {
 		},
 		onDragMove: function (event) {
 			if (!this.drag.isDown) return;
+			
 			const dragX = event.clientX - this.drag.startX;
-
 			this.drag.deltaX = dragX;
 
 			const currentOffset = this.drag.startPosition - dragX;
@@ -248,19 +247,19 @@ window.component.commonCarousel = (function () {
 			this.drag.isDown = false;
 
 			const microSlideRatio = 30 / 100;
-			const threshold = this.els.slideWidth * microSlideRatio;
+			const threshold = this.state.slideWidth * microSlideRatio;
 
 			if (Math.abs(this.drag.deltaX) >= threshold) {
 				const dir = this.drag.deltaX < 0 ? 1 : -1; 
 				let next = this.activeIndex + dir;
-				const lastToFirst = dir === 1 && this.activeIndex === this.els.lastIndex;
+				const lastToFirst = dir === 1 && this.activeIndex === this.state.lastIndex;
 				const firstToLast = dir === -1 && this.activeIndex === 0;
 
 				if (this.options.loop) {
 					if (firstToLast || lastToFirst) {
 						this.carouselEventList.moveToClone.call(this, firstToLast? 'prev' : 'next', lastToFirst); return;
 					};
-				} else if (next < 0 || next > this.els.lastIndex) {
+				} else if (next < 0 || next > this.state.lastIndex) {
 					this.carouselEventList.moveToTransform.call(this);
 					this.drag.deltaX = 0;
 					return;
@@ -270,9 +269,10 @@ window.component.commonCarousel = (function () {
 					this.activeIndex = next;
 					this.carouselEventList.slideChange.call(this);
 				}
+			} else {
+				this.carouselEventList.moveToTransform.call(this);
 			}
 
-			this.carouselEventList.moveToTransform.call(this);
 			this.drag.deltaX = 0;
 		}
 	};
@@ -280,7 +280,7 @@ window.component.commonCarousel = (function () {
 	fn.accessibility = {
 		updateDisabledArrow: function () {
 			const activeFirstSlide = 0 === this.activeIndex;
-			const activeLastSlide = this.els.lastIndex === this.activeIndex;
+			const activeLastSlide = this.state.lastIndex === this.activeIndex;
 
 			this.els.carouselPrevArrow.setAttribute('aria-disabled', activeFirstSlide ? 'true' : 'false');
 			this.els.carouselNextArrow.setAttribute('aria-disabled', activeLastSlide ? 'true' : 'false');
@@ -301,19 +301,17 @@ window.component.commonCarousel = (function () {
 	fn.destroy = function () {
 		this.eventHandler.off.call(this)
 		this.els.carouselWrap.style.transform = '';
+
 		for (let i = 0; i < this.els.carouselSlides.length; i++) {
 			this.els.carouselSlides[i].classList.remove('is-active');
 		};
 		if (this.els.carouselPagination) {
-			for (let i = 0; i < this.els.carouselPaginationBullet.length; i++) {
-				this.els.carouselPaginationBullet[i].classList.remove('is-active');
-			}
+			this.els.carouselPagination.innerHTML = '';
 		};
 		if (this.els.carouselNavigation) {
 			this.els.carouselNextArrow.removeAttribute('aria-disabled');
 			this.els.carouselPrevArrow.removeAttribute('aria-disabled');
 		};
-		if (this.els.carouselPagination) this.els.carouselPagination.innerHTML = '';
 		this.activeIndex = 0;
 		this.els.carouselPaginationBullet = null;
 		this.drag.isDown = false;
